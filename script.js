66ccc6b6c7feaa91e72a815b980e85e903cf626f
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-20 19:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let bro=window.navigator;
+
+let ele=document.getElementById("browser-info");
+
+ele.innerText="You are using "+bro.appName+" version "+bro.appVersion;
